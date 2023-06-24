43ca9b5b2b1ff063ea3e3c6428238b6454f5ddf2
Mukesh <mukeshdixena123@gmail.com>
2023-06-24 14:33:55 +0530

    first commit

---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,37 @@
+
+
+
+// import { Drawer, styled} from '@mui/material';
+import { Drawer } from '@mui/material';
+
+import SideBarContent from './SideBarContent';
+
+
+const SideBar = ({openDrawer}) => {
+    return (
+        <Drawer
+            anchor='left'
+            open={openDrawer}
+            hideBackdrop={true}
+
+            ModalProps={{
+                keepMounted: true
+            }}
+            variant='parsistent'
+            sx={{
+                '& .MuiDrawer-paper': {
+                    marginTop: '64px',
+                    width: 250,
+                    background: '#F5F5F5',
+                    height: 'calc(100vh - 64px)'
+
+                }
+            }}
+        >
+            <SideBarContent />
+        </Drawer>
+    )
+}
+
+
+export default SideBar;
